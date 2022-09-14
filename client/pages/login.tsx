ef75c9e4d22a9cef083fe6f406fcb297094f2c40
Vlad Dubovski <vladislav.dubovski@gmail.com>
2022-09-14 14:37:23 +0300

    added next.js styled-components

---
+++ client/pages/login.tsx
@@ -0,0 +1,8 @@
+import LoginPage from '@/features/pages/Login'
+
+const Login = () => {
+  let a = false
+  return <LoginPage />
+}
+
+export default Login
---
+++ client/pages/registration.tsx
@@ -0,0 +1,7 @@
+import Registration from '@/features/pages/Registration'
+
+function registration() {
+  return <Registration />
+}
+
+export default registration
